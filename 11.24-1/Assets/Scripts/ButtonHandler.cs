804801dce5815658f8cc46aed289cef30091c085
JeYoung <Jeyoung9618@gmail.com>
2019-11-25 01:16:52 +0900

    추가

---
+++ 11.24-1/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void SetReStart()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().ReStartGame();
+    }
+
+    public void SetNextStage()
+    {
+        Debug.Log("다음 스테이지");
+    }
+
+    public void SetExit()
+    {
+        Debug.Log("게임 종료");
+    }
+}
---
+++ 11.24-1/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    int coinCount;
+
+    public Text ScoreUI;
+
+    [SerializeField] GameObject go_ClearUI;
+    [SerializeField] GameObject go_DeathUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCount = 0;
+    }
+
+    public void GetScore()
+    {
+        coinCount++;
+        ScoreUI.text = "Score : " + (int) coinCount;
+    }
+
+    public void ShowClearUI()
+    {
+        go_ClearUI.SetActive(true);
+    }
+
+    public void ShowDeathUI()
+    {
+        go_DeathUI.SetActive(true);
+    }
+
+    public void ReStartGame()
+    {
+        Application.LoadLevel("test");
+    }
+}
