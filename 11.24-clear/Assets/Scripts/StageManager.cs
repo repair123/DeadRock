7af2500b4be0b02615979330818bfad198dbd83b
JeYoung <Jeyoung9618@gmail.com>
2019-11-24 17:03:42 +0900

    스테이지 클리어 코인 추가(플레이어 태그 untagged 에서 Player로 변경) 간단한 UI 추가

---
+++ 11.24-clear/Assets/Scripts/StageManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    [SerializeField] GameObject go_UI;
+
+    public void ShowClearUI()
+    {
+        go_UI.SetActive(true);
+    }
+}
